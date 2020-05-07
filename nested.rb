6f5b3464784d56b4103c2ac919ab3b683f838e08
Rene Avila <reneavila1993@gmail.com>
2020-05-07 19:12:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,21 +1,6 @@
 
 def hopper
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-return programmer_hash[:grace_hopper]
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
